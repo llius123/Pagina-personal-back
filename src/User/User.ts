c49a6c0be8c813938af8d0434863b0573c2c8a81
Jesus Berisa Nuñez <33457979+llius123@users.noreply.github.com>
2020-08-11 21:57:09 +0200

    Login

--- src/User/User.ts
+++ src/User/User.ts
@@ -1,13 +1,21 @@
+import { envConstants } from './../Utils/env.constants';
 import { Prisma } from '../Utils/prisma';
 import { Router } from 'express';
+import jwt from 'jsonwebtoken';
 
 export const User = Router();
 const prisma = Prisma();
 
 User.post('/login', async (req, res) => {
+  console.log(req.body);
   const users = await prisma.uSER.findMany({
     where: { USERNAME: req.body.user, PASSWORD: req.body.pass },
     take: 1,
   });
-  res.send(users);
+  if (users) {
+    var token = jwt.sign(users[0], envConstants.JWT, { expiresIn: '30d' });
+    res.send(token);
+  } else {
+    res.send({ msg: 'Error' });
+  }
 });
--- src/Utils/express.server.ts
+++ src/Utils/express.server.ts
@@ -2,24 +2,26 @@ import { envConstants } from './env.constants';
 import express from 'express';
 import bodyParser from 'body-parser';
 import jwt from 'jsonwebtoken';
+var cookieParser = require('cookie-parser');
 
 export const createApp = () => {
   const app = express();
 
   app.use(bodyParser.urlencoded({ extended: false }));
   app.use(bodyParser.json());
+  app.use(cookieParser());
 
   return app;
 };
 
 export const rutasProtegidas = express.Router();
 rutasProtegidas.use((req, res, next) => {
-  const token = req.headers['access-token'];
+  const token = req.cookies.accesToken;
 
   if (token) {
     jwt.verify(token, envConstants.JWT, (err, decoded) => {
       if (err) {
-        return res.json({ mensaje: 'Token inválida' });
+        return res.json({ msg: 'Token inválida' });
       } else {
         req.body.token = decoded;
         next();
@@ -27,7 +29,7 @@ rutasProtegidas.use((req, res, next) => {
     });
   } else {
     res.send({
-      mensaje: 'Token no proveída.',
+      msg: 'Token no proveída.',
     });
   }
 });
