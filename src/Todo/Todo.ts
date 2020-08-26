ed98f9b5a6d1a13f1937f8e04b58ba1c928712bd
Jesus Berisa Nuñez <33457979+llius123@users.noreply.github.com>
2020-08-26 10:43:27 +0200

    Update todo

--- src/Todo/Todo.ts
+++ src/Todo/Todo.ts
@@ -16,8 +16,8 @@ export class Todo {
   createTodo() {
     return this.router.post('/todo', async (req, res) => {
       const todo = await this.todoSequalize.create({
-        TITLE: req.body.title,
-        PROJECT_ID: req.body.project_id,
+        TITLE: req.body.TITLE,
+        PROJECT_ID: req.body.PROJECT_ID,
       });
       res.send({ todo });
     });
@@ -27,19 +27,19 @@ export class Todo {
     return this.router.put('/todo', async (req, res) => {
       await this.todoSequalize.update(
         {
-          DONE: req.body.done,
-          TITLE: req.body.title,
+          DONE: req.body.DONE,
+          TITLE: req.body.TITLE,
         },
         {
           where: {
-            ID: req.body.id,
+            ID: req.body.ID,
           },
         }
       );
       const todo = await this.todoSequalize.findAll({
         attributes: ['ID', 'TITLE', 'DONE'],
         where: {
-          id: req.body.id,
+          id: req.body.ID,
         },
       });
       res.send({ todo });
@@ -47,10 +47,10 @@ export class Todo {
   }
 
   deleteTodo() {
-    return this.router.delete('/todo', async (req, res) => {
+    return this.router.delete('/todo/:id', async (req, res) => {
       const todo = await this.todoSequalize.destroy({
         where: {
-          ID: req.body.id,
+          ID: req.params.id,
         },
       });
       res.send({ todo });
